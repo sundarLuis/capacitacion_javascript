7ce522be2e47db6d541d4c2882d2cee0ced35624
sundarLuis <sundarjuegos@gmail.com>
2020-08-11 00:46:34 +0000

    add forder funciones

---
+++ 3_funciones/index.js
@@ -0,0 +1,34 @@
+// funcion simple
+function typeOne() {
+    return true
+}
+console.log("typeOne", typeOne())
+/* funcion simple con parametro */
+function typeOneWithParam(param) {
+    if (param) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log("typeOneWithParam", typeOneWithParam("test"))
+/* funcion como variable y simple con parametro */
+const newFormat = function () {
+    return true
+}
+console.log("newFormat", newFormat())
+/* funcion como variable y simple con parametro */
+const newFormatWithParam = function (param) {
+    if (param) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log("newFormatWithParam", newFormatWithParam("test"))
+/* funcion de fecha/arrow function  */
+const arrowFunction = () => true
+console.log("arrowFunction", arrowFunction())
+/* funcion de fecha/arrow function con parametro */
+const arrowFunctionWithParam = (param) => param ? true : false
+console.log("arrowFunctionWithParam", arrowFunctionWithParam("true"))
